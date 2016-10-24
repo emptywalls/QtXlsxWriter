bf38534d7423882b23514a0fc8dce36870cbb10c
Gediminas Veiverys <gediminas.veiverys@intel.com>
2016-10-24 12:36:57 +0200

    saving cells of type Cell::ErrorType

--- src/xlsx/xlsxworksheet.cpp
+++ src/xlsx/xlsxworksheet.cpp
@@ -1365,6 +1365,11 @@ void WorksheetPrivate::saveXmlCellData(QXmlStreamWriter &writer, int row, int co
         writer.writeAttribute(QStringLiteral("t"), QStringLiteral("b"));
         writer.writeTextElement(QStringLiteral("v"), cell->value().toBool() ? QStringLiteral("1") : QStringLiteral("0"));
     }
+    else if (cell->cellType() == Cell::ErrorType) {
+      writer.writeAttribute(QStringLiteral("t"), QStringLiteral("e"));
+      if (cell->hasFormula())
+        cell->formula().saveToXml(writer);
+    }
     writer.writeEndElement(); //c
 }
 
